361559cbc3edace654d4a9c5e5996ce923884cad
Natalia Repkina <pn.88@mail.ru>
2025-07-27 10:22:46 +0300

    feat: add effect for parentState in device

--- smart-home-ui/src/app/components/device/device.component.ts
+++ smart-home-ui/src/app/components/device/device.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, effect, Input, OnInit, Signal, signal } from '@angular/core';
 import { Toggler } from '../../models/common-models';
 import { IconMapperPipe } from '../../pipes/icon-mapper.pipe';
 import { BaseItemComponent } from '../base-item/base-item.component';
@@ -12,20 +12,46 @@ import { ToggleComponent } from '../toggler/toggler.component';
   styleUrl: './device.component.scss',
 })
 export class DeviceComponent extends BaseItemComponent implements OnInit {
+  @Input() parentState!: Signal<boolean>;
+
   toggler = signal<Toggler>({ state: false });
+
+  isInitialized = false;
+
+  constructor() {
+    super();
+
+    effect(
+      () => {
+        const currentState = this.parentState();
+
+        if (this.isInitialized) {
+          this.handleParentStateChange(currentState);
+        } else {
+          this.isInitialized = true;
+        }
+      },
+      { allowSignalWrites: true },
+    );
+  }
 
   ngOnInit() {
     if (this.itemData.state) {
       this.toggler.set({ state: this.itemData.state });
     }
-
-    this.logItemInfo();
   }
 
   onToggleChange() {
     this.toggler.update((current) => {
       if (!current) return current;
       return { ...current, state: !current.state };
+    });
+  }
+
+  private handleParentStateChange(currentState: boolean): void {
+    this.toggler.update((current) => {
+      if (!current) return current;
+      return { ...current, state: currentState };
     });
   }
 }
