2fbfc014ade9ae172f9dcb576d6e70034d9891aa
Natalia Repkina <pn.88@mail.ru>
2025-10-20 12:03:38 +0300

    feat: add content for 404

--- smart-home-ui/src/app/components/not-found/not-found.component.ts
+++ smart-home-ui/src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+  private router = inject(Router);
 
 }
